e37cecc5b5cc262e5fd5110e50fe63e36a9a3742
Thibault JP <thibault.jeanpierre@homepilot.fr>
2022-12-02 22:13:57 +0100

    Add Simple Middleware for Module Cats

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { CatsModule } from './cats/cats.module';
 import { DogsModule } from './dogs/dogs.module';
+
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+
 @Module({
   imports: [DogsModule, CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  // Logger Middleware
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('cats');
+  }
+}
